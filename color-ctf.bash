1bd6598dbc9ab32b66084c7896c36b52ec0a1fb3
Ben Samtleben <38819811+bsamtleben@users.noreply.github.com>
2022-06-19 17:14:06 +0200

    Added main script and first machine (aqua)

---
+++ color-ctf.bash
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+
+LOGFILE=vagrant.log
+
+declare -A MACHINES
+MACHINES[aqua]=192.168.60.2
+
+banner() {
+
+    cat <<EOF
+
+ ██████╗ ██████╗ ██╗      ██████╗ ██████╗      ██████╗████████╗███████╗
+██╔════╝██╔═══██╗██║     ██╔═══██╗██╔══██╗    ██╔════╝╚══██╔══╝██╔════╝
+██║     ██║   ██║██║     ██║   ██║██████╔╝    ██║        ██║   █████╗
+██║     ██║   ██║██║     ██║   ██║██╔══██╗    ██║        ██║   ██╔══╝
+╚██████╗╚██████╔╝███████╗╚██████╔╝██║  ██║    ╚██████╗   ██║   ██║
+ ╚═════╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝     ╚═════╝   ╚═╝   ╚═╝
+
+EOF
+
+}
+
+usage() {
+    cat <<EOF
+There are the following commands:
+    status                      - List info about all machines
+    start <machine>             - Start a machine
+    stop  <machine>             - Stop a machine
+    reset <machine>             - Reset a machine
+
+EOF
+    list_machines
+}
+
+list_machines() {
+    echo "The following machines are available:"
+    for m in "${!MACHINES[@]}"; do
+        echo "    $m"
+    done
+}
+
+print_log_notice() {
+    echo "You can check the most recent log in: $(pwd)/$LOGFILE"
+}
+
+print_machine_startup() {
+    cat <<EOF
+
+Your machine is up and running!
+IP address: ${MACHINES[$1]}
+Happy hacking!
+EOF
+}
+
+status() {
+    MACHINE_STATES=$(vagrant global-status --prune | tr -s " ")
+
+    printf '%-20s %-20s %20s\n'  "Machine" "Status" "IP address"
+    echo '--------------------------------------------------------------'
+    for m in "${!MACHINES[@]}"; do
+
+        # Get machine properties
+        MACHINE_STATE=$(echo "$MACHINE_STATES" | grep " $m " | cut -d " " -f 4)
+        MACHINE_IP=${MACHINES[$m]}
+
+        # If there is no state returned by Vagrant, the machine probably was never initialized
+        if [[ ! $MACHINE_STATE ]]; then
+            MACHINE_STATE="not created"
+        fi
+
+        # Only print the IP address when the machine is online to avoid confusion
+        if [[ "$MACHINE_STATE" != "running" ]]; then
+            MACHINE_IP="-"
+        fi
+
+        printf '%-20s %-20s %20s\n'  "$m" "$MACHINE_STATE" "$MACHINE_IP"
+    done
+}
+
+start() {
+    cd machines
+    echo "Starting machine $1..."
+    echo "This might take a while if you start the machine for the first time or after a reset."
+    echo "So don't worry if this screen appears to be stuck..."
+    echo "Feel free to grab a coffee and return in a few minutes!"
+    vagrant up $1 >$LOGFILE && print_machine_startup $1 && return
+    echo "Something went wrong while starting the machine..."
+    print_log_notice
+}
+
+stop() {
+    cd machines
+    echo "Stopping machine $1..."
+    vagrant halt $1 >$LOGFILE && echo "Machine stopped successfully." && return
+    echo "Something went wrong while stopping the machine..."
+    print_log_notice
+}
+
+reset() {
+    stop
+    echo "Removing machine $1..."
+    vagrant destroy $1 -f >$LOGFILE && echo "Machine removed successfully." && return
+    echo "Something went wrong while removing the machine..."
+    print_log_notice
+}
+
+check_machine_name() {
+    if [[ ! "${MACHINES[$1]}" ]]; then
+        echo "Invalid machine name provided: $1"
+        list_machines
+        exit 1
+    fi
+}
+
+# MAIN
+banner
+
+case "$1" in
+"status")
+    status
+    ;;
+"start")
+    check_machine_name "$2"
+    start "$2"
+    ;;
+"stop")
+    check_machine_name "$2"
+    stop "$2"
+    ;;
+"reset")
+    check_machine_name "$2"
+    reset "$2"
+    ;;
+*)
+    usage
+    ;;
+esac
+
---
+++ machines/aqua/init.bash
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# This script performs the following steps:
+#   1. Download and build Apache HTTP Server from source
+#   2. Download and build vulnerable PHP 8.1.0-dev from source
+#   3. Set up a static PHP website and start Apache
+
+
+# Apache HTTP server source code can be downloaded from here: https://httpd.apache.org/download.cgi
+HTTPD_VERSION=2.4.54
+
+# Install requirements for building Apache
+apt-get install -y  git curl libapr1-dev libaprutil1-dev build-essential libpcre3-dev
+
+# Download and unpack Apache source code
+cd /opt
+curl https://dlcdn.apache.org/httpd/httpd-$HTTPD_VERSION.tar.gz --output httpd.tar.gz
+gzip -d httpd.tar.gz
+tar xvf httpd.tar
+mv ./httpd-$HTTPD_VERSION httpd
+rm httpd.tar
+
+# Build Apache
+cd /opt/httpd
+./configure --prefix=/usr/local
+./configure --enable-so --prefix=/usr/local
+make
+make install
+
+# Install requirements for building PHP
+apt-get install -y pkg-config build-essential autoconf bison re2c libxml2-dev libsqlite3-dev zlib1g zlib1g-dev
+
+# Fetch vulnerable PHP version 8.1.0-dev (https://github.com/php/php-src/commit/c730aa26bd52829a49f2ad284b181b7e82a68d7d)
+mkdir /opt/php
+cd /opt/php
+git init
+git remote add origin https://github.com/php/php-src.git
+git fetch --depth 1 origin c730aa26bd52829a49f2ad284b181b7e82a68d7d
+git checkout FETCH_HEAD
+
+# Build PHP
+./buildconf
+./configure --with-apxs2=/usr/local/bin/apxs --with-zlib=/opt/php/ext/zlib
+make -j4
+make install
+/opt/php/libtool --finish /opt/php/libs
+
+# Create php.ini
+cp /opt/php/php.ini-production /usr/local/lib/php.ini
+
+# Configure Apache
+HTTPD_CONF=/usr/local/conf/httpd-php.conf
+cp $FILES_DIR/httpd-php.conf $HTTPD_CONF
+cat <<EOF >> /usr/local/conf/httpd.conf
+ServerName $HOSTNAME
+Include $HTTPD_CONF
+EOF
+
+# Deploy website
+rm -r /usr/local/htdocs
+cp -r $FILES_DIR/htdocs /usr/local/htdocs
+
+# Create Apache service and enable it
+cp $FILES_DIR/apache2.service /etc/systemd/system/apache2.service
+systemctl daemon-reload
+systemctl enable apache2.service
+systemctl start apache2.service
