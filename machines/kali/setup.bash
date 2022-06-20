cd23097b44ad5cff5f3c0659d06e74f9b8679754
Ben Samtleben <38819811+bsamtleben@users.noreply.github.com>
2022-06-20 17:07:42 +0200

    Added configuration to disable display timeout on kali, reverted keyboard layout script

--- machines/kali/setup.bash
+++ machines/kali/setup.bash
@@ -10,7 +10,11 @@ echo "grub-pc hold" | sudo dpkg --set-selections
 apt-get update && apt-get -y dist-upgrade && apt-get -y autoremove
 
 # Set correct keyboard layout
-setxkbmap -model $XKBMODEL -layout $XKBLAYOUT -variant $XKBVARIANT
+KEYBOARD_FILE=/etc/default/keyboard
+sed -ie '/^XKBMODEL=/s/".*"/"'$XKBMODEL'"/' $KEYBOARD_FILE
+sed -ie '/^XKBLAYOUT=/s/".*"/"'$XKBLAYOUT'"/' $KEYBOARD_FILE
+sed -ie '/^XKBVARIANT=/s/".*"/"'$XKBVARIANT'"/' $KEYBOARD_FILE
+udevadm trigger --subsystem-match=input --action=change
 
 # Set time zone
 timedatectl set-timezone $TIMEZONE
@@ -22,7 +26,7 @@ usermod -a -G sudo "$USERNAME"
 chown -R "$USERNAME":"$USERNAME" "$HOME_DIR"
 
 # ZSH configuration
-cat > "$HOME_DIR/.zshrc-custom" <<- EOF
+cat <<EOF > "$HOME_DIR/.zshrc-custom"
 # Some ls aliases
 alias ll='ls -lah'
 alias la='ls -lah'
@@ -31,6 +35,27 @@ EOF
 
 # Include custom zshrc
 echo 'source ~/.zshrc-custom' >> "$HOME_DIR/.zshrc"
+
+# Disable display timeout
+PERCHANNEL_DIR=/home/$USERNAME/.config/xfce4/xfconf/xfce-perchannel-xml
+mkdir -p $PERCHANNEL_DIR
+cat <<EOF > $PERCHANNEL_DIR/xfce4-power-manager.xml
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xfce4-power-manager" version="1.0">
+  <property name="xfce4-power-manager" type="empty">
+    <property name="power-button-action" type="empty"/>
+    <property name="show-panel-label" type="empty"/>
+    <property name="show-tray-icon" type="bool" value="false"/>
+    <property name="lock-screen-suspend-hibernate" type="bool" value="false"/>
+    <property name="logind-handle-lid-switch" type="bool" value="false"/>
+    <property name="dpms-enabled" type="bool" value="true"/>
+    <property name="blank-on-ac" type="int" value="0"/>
+    <property name="dpms-on-ac-sleep" type="uint" value="0"/>
+    <property name="dpms-on-ac-off" type="uint" value="0"/>
+  </property>
+</channel>
+EOF
 
 # Transfer file ownership
 chown -R "$USERNAME":"$USERNAME" "$HOME_DIR"
