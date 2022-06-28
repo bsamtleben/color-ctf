0755dc2da645ca5e7240a24a4b7f52ab5388b6b1
Ben Samtleben <git@jupiterspace.de>
2022-06-28 13:07:18 +0200

    Fix: SSH command responded with non-zero exit code

--- machines/_general/post.bash
+++ machines/_general/post.bash
@@ -3,4 +3,7 @@
 # Refresh access and modification times for all files
 # => prevent an attacker from simply looking up which files were changed most recently
 # Note that this method is not 100% perfect since the "birth date" cannot be changed easily without messing with inodes (see debugfs for more info)
-find / -exec touch -a -m {} + 2>/dev/null
+find /etc /home /opt /root /srv /usr /var -exec touch -a -m {} + 2>/dev/null
+
+# Delete this shell script from /tmp
+rm /tmp/vagrant-shell
