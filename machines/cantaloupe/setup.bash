438510f78222ae2b65e07d55a734f6ffd66094a7
Ben Samtleben <git@jupiterspace.de>
2022-06-30 22:40:41 +0200

    First part of cantaloupe setup implemented

--- machines/cantaloupe/setup.bash
+++ machines/cantaloupe/setup.bash
@@ -7,6 +7,7 @@ USERNAME=cantaloupe
 
 # This script performs the following steps:
 #   0. Place the tokens
+#   1. Set up FTP server (vsftpd)
 
 # Create non-root user (without password)
 useradd -m $USERNAME -s /bin/bash
@@ -21,4 +22,15 @@ chmod 644 $HOME_DIR/token
 # Put the second token in root's home directory
 echo "Well done, my friend: $TOKEN2" > /root/token
 
+# Install vsftpd and copy configuration file
+apt-get -y install vsftpd
+cp $FILES_DIR/ftp/vsftpd.conf /etc/vsftpd.conf
 
+# Create FTP data directory and copy files
+mkdir -p /var/ftp
+cp -r $FILES_DIR/ftp/public /var/ftp/public
+chown -R nobody:nogroup /var/ftp/public
+
+# Enable and start vsftpd service
+systemctl start vsftpd
+systemctl enable vsftpd
