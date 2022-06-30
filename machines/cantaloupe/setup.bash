882c44c4043bd2fc2d0ed16731f2fc4c09e26962
Ben Samtleben <git@jupiterspace.de>
2022-07-01 00:28:16 +0200

    Second part of cantaloupe setup completed

--- machines/cantaloupe/setup.bash
+++ machines/cantaloupe/setup.bash
@@ -8,6 +8,7 @@ USERNAME=cantaloupe
 # This script performs the following steps:
 #   0. Place the tokens
 #   1. Set up FTP server (vsftpd)
+#   2. Set up web server (Apache with PHP)
 
 # Create non-root user (without password)
 useradd -m $USERNAME -s /bin/bash
@@ -32,5 +33,17 @@ cp -r $FILES_DIR/ftp/public /var/ftp/public
 chown -R nobody:nogroup /var/ftp/public
 
 # Enable and start vsftpd service
-systemctl start vsftpd
+systemctl restart vsftpd
 systemctl enable vsftpd
+
+# Install Apache web server and PHP
+apt-get install -y apache2 php libapache2-mod-php
+
+# Deploy web contents
+rm -r /var/www/html
+cp -r $FILES_DIR/html /var/www/html
+chown -R www-data:www-data /var/www/html
+
+# Start and enable Apache service
+systemctl restart apache2
+systemctl enable apache2
